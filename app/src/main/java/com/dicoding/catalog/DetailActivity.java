38ace98834e54175000d424be8bcd2138d6a3026
majidilkhithar <nuralimajids@gmail.com>
2020-01-12 23:36:23 +0700

    u

--- app/src/main/java/com/dicoding/catalog/DetailActivity.java
+++ app/src/main/java/com/dicoding/catalog/DetailActivity.java
@@ -1,4 +1,6 @@
 package com.dicoding.catalog;
 
 public class DetailActivity {
+    public static final String EXTRA_POSITION = "extra_position";
+    public static final String EXTRA_MOVIE = "extra_movie";
 }
--- app/src/main/java/com/dicoding/catalog/adapter/ListMovieAdapter.java
+++ app/src/main/java/com/dicoding/catalog/adapter/ListMovieAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.dicoding.catalog.DetailActivity;
 import com.dicoding.catalog.R;
+import com.dicoding.catalog.entity.Movie;
 
 import java.util.ArrayList;
 
@@ -41,14 +41,14 @@ public class ListMovieAdapter extends RecyclerView.Adapter<ListMovieAdapter.List
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie,parent,false);
-        return view;
+        return new ListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
         Movie movie = listMovie.get(position);
-        Glide.with(holder.itemView.getContex())
-                .load(movie.getPhoto)
+        Glide.with(holder.itemView.getContext())
+                .load(movie.getPhoto())
                 .into(holder.imgPhoto);
 
         holder.tvTitle.setText(movie.getTitle());
