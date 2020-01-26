2c7860ec59255a367b825b5d509f5b8130757594
majidilkhithar <nuralimajids@gmail.com>
2020-01-26 23:21:23 +0700

    up

--- app/src/main/java/com/dicoding/catalog/design/FavFragment.java
+++ app/src/main/java/com/dicoding/catalog/design/FavFragment.java
@@ -1,9 +1,81 @@
 package com.dicoding.catalog.design;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.dicoding.catalog.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavFragment extends Fragment {
 
-    public FavFragment(){}\
-    
+    public FavFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(v, savedInstanceState);
+        ViewPager vPager= v.findViewById(R.id.viewpager);
+        setupViewPager(vPager);
+
+        TabLayout tabL = v.findViewById(R.id.tabs);
+        tabL.setupWithViewPager(vPager);
+    }
+
+    private void setupViewPager(ViewPager vPager) {
+        ViewPgAdapter Adapter = new ViewPgAdapter(getChildFragmentManager());
+        Adapter.addFragment(new MovFavorite(),getString(R.string.movies));
+        vPager.setAdapter(Adapter);
+    }
+
+
+    class ViewPgAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> listFrag = new ArrayList<>();
+        private final List<String> listFraTitle = new ArrayList<>();
+
+
+
+        ViewPgAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return listFrag.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return listFrag.size();
+        }
+
+        void addFragment(Fragment fm, String title){
+            listFrag.add(fm);
+            listFraTitle.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return listFraTitle.get(position);
+        }
+    }
+
 }
--- app/src/main/java/com/dicoding/catalog/design/MovFavorite.java
+++ app/src/main/java/com/dicoding/catalog/design/MovFavorite.java
@@ -41,7 +41,7 @@ public class MovFavorite extends Fragment implements LoadDataCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_movie_favorite, container, false);
+        return inflater.inflate(R.layout.fragment_moviefavorite, container, false);
     }
 
     @Override
